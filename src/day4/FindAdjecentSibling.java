c34ca898ff26b209fe3799c0b324d1caf61b6d6d
TechnoStudy <info@techno.study>
2019-12-17 20:28:08 -0500

    Day 4, type

--- src/day4/FindAdjecentSibling.java
+++ src/day4/FindAdjecentSibling.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FindAdjecentSiblong {
+public class FindAdjecentSibling {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
