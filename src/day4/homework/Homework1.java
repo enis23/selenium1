e0ebe53ac8b1541b074516b8f667357df7935edc
Apps at Techno Study <apps@techno.study>
2019-12-18 11:54:05 -0500

    Homework for day 4

---
+++ src/day4/homework/Homework1.java
@@ -0,0 +1,11 @@
+package day4.homework;
+
+public class Homework1 {
+
+    // go to URL: https://www.seleniumeasy.com/test/
+    // click on each link of class="board-inner"
+    // and validate that class="tab-content" has correct text in active "h3" tag and adjacent "p" tag
+    // after you click associated link above
+
+    // there should be at least 5 test of "h3" tag and adjacent "p" tag
+}
