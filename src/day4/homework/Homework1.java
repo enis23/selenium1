69085ab86e79bbe73e0abf71c0320585488d4daf
TechnoStudy <info@techno.study>
2019-12-19 21:55:29 -0500

    Day 4, solution first part

--- src/day4/homework/Homework1.java
+++ src/day4/homework/Homework1.java
@@ -1,5 +1,12 @@
 package day4.homework;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class Homework1 {
 
     // go to URL: https://www.seleniumeasy.com/test/
@@ -7,6 +14,31 @@ public class Homework1 {
     // and validate that class="tab-content" has
     // correct text for "h3" tag and adjacent "p" tag in active div
     // after you click associated link above
-
+        // h3 + p
     // there should be at least 5 test of "h3" tag and adjacent "p" tag
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://www.seleniumeasy.com/test/" );
+        List<WebElement> links = driver.findElements( By.cssSelector( ".board-inner a" ) );
+        for(WebElement link : links) {
+            link.click();
+            Thread.sleep( 1000 );
+            WebElement h3Tag = driver.findElement( By.cssSelector( ".tab-content .active h3" ) );
+            String h3 = "WELCOME TO SELENIUM EASY DEMO";
+            final boolean h3Validation = h3Tag.getText().equals( h3 );
+
+            WebElement pTag = driver.findElement( By.cssSelector( ".tab-content .active h3 + p" ) );
+            String p = "We have listed most of the components that are used by developers to build web applications.";
+            final boolean pValidation = pTag.getText().equals( p );
+
+            if(h3Validation && pValidation) {
+                System.out.println("Success!");
+            } else {
+                System.out.println("Failure!");
+            }
+        }
+
+
+    }
 }
