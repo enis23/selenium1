1a6c8fe4a5b8ebdb446fb1505bd45d6c8feafc92
TechnoStudy <info@techno.study>
2019-12-21 12:27:35 -0500

    Day 6, find by xpath

---
+++ src/day6/FindByXPath.java
@@ -0,0 +1,9 @@
+package day6;
+
+public class FindByXPath {
+    // https://www.seleniumeasy.com/test/input-form-demo.html
+    // fill in the form using xpath by attribute,
+    // by contains(),
+    // by index,
+    // also by child
+}
