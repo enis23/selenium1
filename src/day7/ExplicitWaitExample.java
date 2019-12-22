1be4f6b6b76ae8b3ec3d912d1da1b11455662902
Apps at Techno Study <apps@techno.study>
2019-12-22 09:14:51 -0500

    Day 7

---
+++ src/day7/ExplicitWaitExample.java
@@ -0,0 +1,22 @@
+package day7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWaitExample {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty( "webdriver.chrome.driver", "/Users/technostudy/Selenium/ChromeDriver/chromedriver" );
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://jsfiddle.net/daulet2030/2qfux1r7/" );
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        driver.switchTo().frame("result");
+//        WebElement webElement = driver.findElement(By.cssSelector("#test1")); //this would give us exception
+        WebElement webElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#test1")));
+        System.out.println(webElement.getTagName());
+//        driver.quit();
+    }
+}
---
+++ src/day7/ExplicitWaitVisibilityExample.java
@@ -0,0 +1,21 @@
+package day7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWaitVisibilityExample {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty( "webdriver.chrome.driver", "/Users/technostudy/Selenium/ChromeDriver/chromedriver" );
+        WebDriver driver = new ChromeDriver();
+        driver.get( "https://jsfiddle.net/daulet2030/eLy027xg/" );
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        driver.switchTo().frame("result");
+        WebElement webElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#test1")));
+        System.out.println(webElement.getTagName());
+//        driver.quit();
+    }
+}
