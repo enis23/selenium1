af7a3c9781fbd8a83ad35dd68631701f526dea0a
TechnoStudy <info@techno.study>
2019-12-22 16:32:57 -0500

    Day 7, Task 4

--- src/day7/tasks/Task4.java
+++ src/day7/tasks/Task4.java
@@ -4,5 +4,5 @@ public class Task4 {
     // https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html
     // click on the "Autocloseable success message" button
     // then validate that "I'm an autocloseable success  message. I will hide in 5 seconds."
-    // disappears, wait at least 10 seconds
+    // disappears, wait at least 10 seconds for it to disappear
 }
