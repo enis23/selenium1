38679b03d6d5610c39592cc4bb92f05e2633b7d6
TechnoStudy <info@techno.study>
2019-12-24 19:24:44 -0500

    Day 9, Corrected Scrolling

--- src/day9/ScrollIntoView.java
+++ src/day9/ScrollIntoView.java
@@ -16,19 +16,20 @@ public class ScrollIntoView {
         WebDriver driver = new ChromeDriver();
         driver.get( "https://ultimateqa.com/automation/fake-pricing-page" );
 
-//        WebDriverWait wait = new WebDriverWait( driver, 15 );
-//        WebElement h4 = wait.until( ExpectedConditions.visibilityOfElementLocated( By.cssSelector( ".et_pb_blurb_0 h4" ) ) );
         WebElement h4 = driver.findElement( By.cssSelector( ".et_pb_blurb_0 h4" ) );
 
         // Scroll to h4 element
         ((JavascriptExecutor) driver).executeScript( "arguments[0].scrollIntoView(true);", h4 );
-        String testH4 = "Help Finding Information Online?";
 
+        WebDriverWait wait = new WebDriverWait( driver, 15 );
+        wait.until( ExpectedConditions.visibilityOfElementLocated( By.cssSelector( ".et_pb_blurb_0 h4" ) ) );
+
+        String testH4 = "Help Finding Information Online?";
         System.out.println( "Tag name:" + h4.getTagName() );
         System.out.println( "Displayed:" + h4.isDisplayed() );
-        System.out.println( "Text is:" +  h4.getText() );
+        System.out.println( "Text is:" + h4.getText() );
 
-        System.out.println(h4.getText().equals( testH4 ) ? "Success": "Failure");
+        System.out.println( h4.getText().equals( testH4 ) ? "Success" : "Failure" );
 
         driver.quit();
     }
