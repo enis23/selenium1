5a7bce7025ac1093bbc7f57b9c796f5744863708
TechnoStudy <info@techno.study>
2019-12-24 19:16:02 -0500

    Day 9, Homework

---
+++ src/day9/homework/homework2/SignUpForm.java
@@ -0,0 +1,8 @@
+package day9.homework.homework2;
+
+public class SignUpForm {
+    /**
+     * open SignUpForm.html
+     * fill in your data inside the all input elements
+     */
+}
---
+++ src/day9/homework/homework2/SignUpFormSubmissionParsing.java
@@ -0,0 +1,11 @@
+package day9.homework.homework2;
+
+public class SignUpFormSubmissionParsing {
+    /**
+     * open SignUpForm.html
+     * fill in your data inside the all input elements
+     * submit the form
+     * get URL
+     * parse values that was submitted and print them out
+     */
+}
