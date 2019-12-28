4e447a1c22dc450d06aaef4e38f7ba4b658524e0
TechnoStudy <info@techno.study>
2019-12-28 14:10:44 -0500

    day 9, hw2.1 solution

---
+++ src/day9/homework/homework2/SignUpFormSolution.java
@@ -0,0 +1,25 @@
+package day9.homework.homework2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class SignUpFormSolution {
+    public static void main(String[] args) {
+        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+        WebDriver driver = new ChromeDriver();
+        String path = "file:///" + System.getProperty( "user.dir" ) + "/src/day9/homework/homework1/SignUpFormSolution.html";
+        driver.get( path );
+        driver.findElement( By.name( "username" ) ).sendKeys( "name" );
+        driver.findElement( By.name( "password" ) ).sendKeys( "password" );
+        driver.findElement( By.name( "password_repeat" ) ).sendKeys( "password" );
+        driver.findElement( By.name( "address" ) ).sendKeys( "7514 Riverview Rd.\n" + "Sheboygan, WI 53081" );
+        driver.findElement( By.cssSelector( "input[type='radio'][value='male']" ) ).click();
+        Select plan = new Select( driver.findElement( By.name( "plan" ) ) );
+        plan.selectByIndex( plan.getOptions().size() - 1 ); //selects last option
+        driver.findElement( By.name( "agreement" ) ).click();
+//        driver.quit();
+    }
+}
