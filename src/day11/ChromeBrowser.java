0b2587d70e0ee4ff09042e22526c280b1f9fa521
TechnoStudy <info@techno.study>
2019-12-29 14:16:54 -0500

    day 10, notes

--- src/day11/ChromeBrowser.java
+++ src/day11/ChromeBrowser.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ChromeBrowser {
     public static void main(String[] args) {
-        System.setProperty( "webdriver.chrome.driver", "/Users/technostudy/Selenium/ChromeDriver/chromedriver" );
+        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
         driver.get("https://test-basqar.mersys.io");
         driver.manage().window().maximize();
@@ -23,7 +23,6 @@ public class ChromeBrowser {
         driver.findElement(By.xpath("//span[contains(text(),'Entrance Exams')]")).click();
         driver.findElement(By.xpath("//span[contains(text(),'Entrance Exams')]/../..//span[contains(text(),'Setup')]")).click();
         driver.findElement(By.xpath("//span[contains(text(),'Setup')]/../..//span[contains(text(),'Entrance Exams')]")).click();
-
         WebDriverWait wait = new WebDriverWait( driver, 15 );
         try {
             wait.until( ExpectedConditions.presenceOfElementLocated( By.cssSelector( "tbody > tr" ) ) ); //this is to wait for the first search
@@ -35,7 +34,7 @@ public class ChromeBrowser {
         driver.findElement(By.xpath("//span[contains(text(),'Search')]")).click();
 
         try {
-            wait.until( ExpectedConditions.presenceOfElementLocated( By.xpath("//td[contains(@class,'name') and contains(text(), '12')]") ) );
+            wait.until( ExpectedConditions.presenceOfElementLocated( By.xpath("//td[contains(@class,'name') and contains(text(), '121231233')]") ) );
             System.out.println("Success!");
         } catch (Exception e){
             System.out.println("Failure!");
