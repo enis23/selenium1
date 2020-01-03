9290797514df2210bccbed832036be04abb7e359
TechnoStudy <info@techno.study>
2020-01-02 20:42:57 -0500

    priority

--- src/day13/examples/dependsOnMethod/PriorityTestCases.java
+++ src/day13/examples/dependsOnMethod/PriorityTestCases.java
@@ -2,25 +2,29 @@ package day13.examples.dependsOnMethod;
 
 import org.testng.annotations.Test;
 
-public class CityTestCases {
-
-    @Test(dependsOnMethods = {"login"})
+public class PriorityTestCases {
+    @Test(priority = 2)
     public void create() {
         System.out.println("City create test case");
     }
 
-    @Test(dependsOnMethods = {"login"})
+    @Test(priority = 3)
     public void edit() {
         System.out.println("City edit test case");
     }
 
-    @Test(dependsOnMethods = {"login", "create"})
+    @Test(priority = 4)
     public void delete() {
         System.out.println("City delete test case");
     }
 
-    @Test
+    @Test(priority = 1)
     public void login() {
         System.out.println("Login");
+    }
+
+    @Test
+    public void zeroPriority(){
+        System.out.println("this is zero priority!");
     }
 }
